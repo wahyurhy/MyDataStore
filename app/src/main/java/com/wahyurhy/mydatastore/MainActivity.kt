fd337ce18c6245a68eb9324fc79040c749326b3b
wahyurhy <wahyurahayu82@gmail.com>
2022-03-18 16:40:17 +0700

    init

---
+++ app/src/main/java/com/wahyurhy/mydatastore/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.wahyurhy.mydatastore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+        }
+    }
+}
